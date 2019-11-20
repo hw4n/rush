f9c9f63cd45d5a39742bb8992a338658e612a891
Hwanhee Chae <prisable1337@gmail.com>
2019-11-20 21:53:19 +0900

    Add chat, chat css

--- app.js
+++ app.js
@@ -16,14 +16,21 @@ app.get("/chat", (req, res) => {
 });
 
 io.sockets.on("connection", (socket) => {
-  console.log("[+] User connected");
+  socket.on("newUser", (name) => {
+    console.log(`[+] User(${name}) connected`);
+    socket.name = name;
+    io.sockets.emit("update", {type: "connect", name: "SERVER", message: name + " 접속"});
+  });
 
-  socket.on("send", (data) => {
-    console.log("[+] User sent msg: " + data.msg);
+  socket.on("message", (data) => {
+    data.name = socket.name;
+    console.log(`[+] User(${data.name}) sent msg: ${data.message}`);
+    socket.broadcast.emit("update", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("[-] User disconnected");
+    console.log(`[-] User(${socket.name}) disconnected`);
+    socket.broadcast.emit("update", {type: "disconnect", name: "SERVER", message: socket.name + " 접속 종료"});
   });
   
 });
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,16 +1,48 @@
 var socket = io();
-var chatbox = document.querySelector("#chatbox");
+var chat = document.querySelector("#chat");
+var chatInput = document.querySelector("#chatInput");
 
 socket.on("connect", () => {
-  chatbox.value = "connected";
+  var name = Math.random().toString(36).substr(2, 7);
+  socket.emit("newUser", name);
+});
+
+socket.on("update", (data) => {
+  var message = document.createElement("div");
+  var text = document.createTextNode(`${data.name}: ${data.message}\n`);
+  var msgClass = "";
+
+  switch (data.type) {
+    case "message":
+      msgClass = ["users"];
+      break;
+    case "connect":
+      msgClass = ["system", "connect"];
+      break;
+    case "disconnect":
+      msgClass = ["system", "disconnect"];
+      break;
+  }
+
+  message.classList.add(...msgClass);
+  message.appendChild(text);
+  chat.appendChild(message);
 });
 
 function send() {
-  socket.emit("send", {msg: chatbox.value});
-  chatbox.value = "";
+  socket.emit("message", {type: "message", message: chatInput.value});
+
+  var message = document.createElement("div");
+  var text = document.createTextNode(chatInput.value);
+
+  message.classList.add("client");
+  message.appendChild(text);
+  chat.appendChild(message);
+  
+  chatInput.value = "";
 }
 
-chatbox.addEventListener("keypress", (e) => {
+chatInput.addEventListener("keypress", (e) => {
   var key = e.which || e.keyCode;
   if (key == 13) // 'Enter'
     send();
